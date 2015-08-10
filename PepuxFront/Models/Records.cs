8c7412eb2192172755e47d2a1e26901b4b9f4310
Alexander Pilugin <skipper-com@outlook.com>
2015-08-10 18:40:57 +0300

    records

---
+++ PepuxFront/Models/Records.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PepuxFront.Models
+{
+    public class Records
+    {
+        public int ID { get; set; }
+        public string Conf { get; set; }
+        public string PName { get; set; }
+        public DateTime Tstart { get; set; }
+        public DateTime Tfinish { get; set; }
+        public string Link { get; set; }
+    }
+}
+}
